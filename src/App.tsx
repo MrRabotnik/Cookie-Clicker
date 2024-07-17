dc0bb2d1e21f02a8d6ca8ac010c4950d79c53443
Sargis <terbalyantss@gmail.com>
2024-07-17 19:56:56 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,134 @@
-import React, { useRef, useEffect, useState } from "react";
-import { Stage, Layer, Image } from "react-konva";
+import React, { useRef, useState, useEffect } from "react";
+import Konva from "konva";
+import { Stage, Layer, Image as KonvaImage, Text, Rect, Group } from "react-konva";
 import useImage from "use-image";
 
 const App = () => {
-    const [cookieImage] = useImage("./assets/cookie.png");
-    const stageRef = useRef(null);
-    const layerRef = useRef(null);
-    const imageRef = useRef(null);
-    const [dimensions, setDimensions] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
-    });
-    const [position, setPosition] = useState({ x: 100, y: 100 });
-    const [direction, setDirection] = useState({ x: 1, y: 1 });
+    const [cookie] = useImage("https://opengameart.org/sites/default/files/styles/medium/public/Cookie.png");
+    const [cookieHalo] = useImage("https://orteil.dashnet.org/cookieclicker/img/shine.png");
+    const [bg] = useImage("https://wallpapercave.com/wp/wp7667538.jpg");
 
-    const handleResize = () => {
-        setDimensions({
-            width: window.innerWidth,
-            height: window.innerHeight,
-        });
-    };
+    const [cookiesCount, setCookiesCount] = useState(0);
+    const [cookiesPerClick, setCookiesPerClick] = useState(1);
+    const [multiplier, setMultiplier] = useState(1);
+
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+
+    const haloRef = useRef<any>(null);
+    const [scrollY1, setScrollY1] = useState(0);
+    const [scrollY2, setScrollY2] = useState(0);
 
     useEffect(() => {
-        window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
+        if (haloRef.current) {
+            const anim = new Konva.Animation((frame: any) => {
+                const angleDiff = (frame.timeDiff * 90) / 5000;
+                haloRef.current.rotate(angleDiff);
+            }, haloRef.current.getLayer());
+
+            anim.start();
+        }
     }, []);
 
-    useEffect(() => {
-        const animate: any = () => {
-            if (!layerRef.current) return;
-
-            // Get the current stage dimensions
-            const stage: any = stageRef.current;
-            const layer: any = layerRef.current;
-            const image: any = imageRef.current;
-            const stageWidth = stage.width();
-            const stageHeight = stage.height();
-
-            // Clear the canvas
-            layer.clear();
-
-            // Update position
-            let newX = position.x + direction.x;
-            let newY = position.y + direction.y;
-
-            // Check for boundaries
-            if (newX <= 0 || newX + image.width() >= stageWidth) {
-                setDirection((prevDirection) => ({ ...prevDirection, x: -prevDirection.x }));
-            }
-            if (newY <= 0 || newY + image.height() >= stageHeight) {
-                setDirection((prevDirection) => ({ ...prevDirection, y: -prevDirection.y }));
-            }
-
-            // Set new position
-            setPosition({ x: newX, y: newY });
-
-            // Request the next frame
-            requestAnimationFrame(animate);
-        };
-
-        // Start the animation loop
-        // animate();
-
-        // Cleanup on unmount
-        return () => cancelAnimationFrame(animate);
-    }, [position, direction]);
+    const handleClickOnCookie = () => {
+        const addedNewCookies = cookiesPerClick * multiplier;
+        setCookiesCount((prev) => prev + addedNewCookies);
+        setCookiesPerClick(addedNewCookies);
+    };
 
     return (
         <Stage
-            width={dimensions.width}
-            height={dimensions.height}
-            ref={stageRef}
+            width={w}
+            height={h}
         >
-            <Layer ref={layerRef}>
-                <Image
-                    image={cookieImage}
-                    x={position.x}
-                    y={position.y}
-                    ref={imageRef}
+            <Layer>
+                <KonvaImage image={bg} />
+                <KonvaImage
+                    image={cookieHalo}
+                    x={w / 3 / 2}
+                    y={h / 2}
+                    width={300}
+                    height={300}
+                    offsetX={150}
+                    offsetY={150}
+                    ref={haloRef}
                 />
+                <KonvaImage
+                    image={cookie}
+                    width={300}
+                    height={220}
+                    x={w / 3 / 2}
+                    y={h / 1.8}
+                    offsetX={150}
+                    offsetY={150}
+                    onClick={handleClickOnCookie}
+                />
+                <Rect
+                    width={w / 3 / 1.5}
+                    height={100}
+                    x={80}
+                    y={150}
+                    fill="white"
+                    opacity={0.5}
+                    cornerRadius={10}
+                />
+                <Text
+                    text={`Cookies ${cookiesCount.toString()}`}
+                    x={150}
+                    y={150}
+                    fill="white"
+                    fontSize={32}
+                    fontFamily="Maname"
+                />
+                <Text
+                    text={`Cookies per second +${cookiesPerClick.toString()}`}
+                    x={120}
+                    y={210}
+                    fill="white"
+                    fontSize={24}
+                    fontFamily="Maname"
+                />
+                <Rect
+                    width={10}
+                    height={h}
+                    x={w / 3}
+                    fill="darkgreen"
+                />
+
+                <Group y={-scrollY1}>
+                    {Array.from({ length: 30 }).map((_, i) => (
+                        <Rect
+                            key={i}
+                            x={w / 3 + 50}
+                            y={i * 110}
+                            width={w / 3 / 1.2}
+                            height={100}
+                            fill="white"
+                            opacity={0.5}
+                            cornerRadius={10}
+                        />
+                    ))}
+                </Group>
+                <Rect
+                    width={10}
+                    height={h}
+                    x={(w / 3) * 2}
+                    fill="darkgreen"
+                />
+                <Group y={-scrollY2}>
+                    {Array.from({ length: 30 }).map((_, i) => (
+                        <Rect
+                            key={i}
+                            x={(w / 3) * 2 + 50}
+                            y={i * 110}
+                            width={w / 3 / 1.2}
+                            height={100}
+                            fill="white"
+                            opacity={0.5}
+                            cornerRadius={10}
+                        />
+                    ))}
+                </Group>
             </Layer>
         </Stage>
     );
