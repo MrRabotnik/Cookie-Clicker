7bcfb087abf0ca61c20ce0c2eccc51b508d14bc0
Sargis <terbalyantss@gmail.com>
2024-07-25 23:53:48 +0400

    Formatting changed up to 10*23

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -6,9 +6,33 @@ numeral.register("format", "largeNumbers", {
         unformat: /0(\.0+)?[a-z]+/,
     },
     format: function (value, format, roundingFunction) {
-        const units = ["", "k", "m", "b", "t", "q", "Q", "s", "S", "o", "n", "d"];
+        const units = [
+            "",
+            "k",
+            "m",
+            "b",
+            "t",
+            "q",
+            "Q",
+            "s",
+            "S",
+            "o",
+            "n",
+            "d",
+            "U",
+            "D",
+            "T",
+            "Qt",
+            "Qd",
+            "Sd",
+            "St",
+            "O",
+            "N",
+            "v",
+            "c",
+        ];
         let unit = "";
-        let power = 1;
+        let power = 0;
 
         while (value >= 1000 && power < units.length - 1) {
             value /= 1000;
@@ -20,7 +44,31 @@ numeral.register("format", "largeNumbers", {
         return output + unit;
     },
     unformat: function (string) {
-        const units = { "": 0, k: 3, m: 6, b: 9, t: 12, q: 15, Q: 18, s: 21, S: 24, o: 27, n: 30, d: 33 };
+        const units = {
+            "": 0,
+            k: 3,
+            m: 6,
+            b: 9,
+            t: 12,
+            q: 15,
+            Q: 18,
+            s: 21,
+            S: 24,
+            o: 27,
+            n: 30,
+            d: 33,
+            U: 36,
+            D: 39,
+            T: 42,
+            Qt: 45,
+            Qd: 48,
+            Sd: 51,
+            St: 54,
+            O: 57,
+            N: 60,
+            v: 63,
+            c: 66,
+        };
         let multiplier = 1;
         let num = numeral._.stringToNumber(string);
 
@@ -38,26 +86,20 @@ export const formatNumber = (number) => {
     let formatted;
 
     if (number % 1 === 0) {
-        // Check if the number is an integer
-        if (number <= 999999 && number >= 0) {
-            // Format numbers between 1 and 999,999 without dividing or adding a letter
+        if (number < 1000000 && number >= 0) {
             formatted = numeral(number).format("0,0");
         } else {
-            // Use custom format for numbers 1,000,000 and above
-            formatted = numeral(number)
-                .format("0a")
-                .replace(/([a-z])$/, "$1");
-        }
-    } else {
-        // If the number has a decimal part
-        if (number <= 999999 && number >= 0) {
-            // Format numbers between 1 and 999,999 without dividing or adding a letter
-            formatted = numeral(number).format("0,0.0");
-        } else {
-            // Use custom format for numbers 1,000,000 and above
             formatted = numeral(number)
                 .format("0.0a")
-                .replace(/([a-z])$/, "$1");
+                .replace(/([a-z]+)$/, "$1");
+        }
+    } else {
+        if (number < 1000000 && number >= 0) {
+            formatted = numeral(number).format("0,0.0");
+        } else {
+            formatted = numeral(number)
+                .format("0.0a")
+                .replace(/([a-z]+)$/, "$1");
         }
     }
 
